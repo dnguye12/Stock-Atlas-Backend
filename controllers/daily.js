d06809633ff18ba89d486148706b0e29ce3acb21
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-06-29 22:37:03 +0200

    daily losers api

--- controllers/daily.js
+++ controllers/daily.js
@@ -36,6 +36,67 @@ dailyRouter.get('/gainers/:count?/:region?', async (request, response) => {
     }
 })
 
+dailyRouter.get('/losers/:count?/', async (request, response) => {
+    let { count } = request.params
+
+    if (!count || isNaN(count)) {
+        count = 5
+    }
+
+    const url = 'https://finance.yahoo.com/losers/'
+
+    try {
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(url, { waitUntil: 'networkidle2' })
+
+        const acceptButtonSelector = '.actions .accept-all';
+        const acceptButton = await page.$(acceptButtonSelector);
+
+        if (acceptButton) {
+            await page.click(acceptButtonSelector);
+            await page.waitForSelector('table')
+            await new Promise(r => setTimeout(r, 2000))
+        } else {
+            console.log('daily/active API - Accept All button not found');
+        }
+
+        const content = await page.evaluate((count) => {
+            const res = []
+            const rows = document.querySelectorAll('table tbody tr.simpTblRow')
+
+            for (let i = 0; i < count && i < rows.length; i++) {
+                const row = rows[i];
+                const cells = row.querySelectorAll('td');
+
+                if (cells.length >= 9) {
+                    const rowObj = {
+                        Symbol: cells[0].textContent.trim(),
+                        Name: cells[1].textContent.trim(),
+                        Price: cells[2].textContent.trim(),
+                        Change: cells[3].textContent.trim(),
+                        PercentChange: cells[4].textContent.trim(),
+                        Volume: cells[5].textContent.trim(),
+                        AvgVol: cells[6].textContent.trim(),
+                        MarketCap: cells[7].textContent.trim(),
+                        PE: cells[8].textContent.trim()
+                    };
+                    res.push(rowObj);
+                }
+
+            }
+            return res
+        }, count)
+        await browser.close()
+        response.json(content)
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+})
+
 dailyRouter.get('/trending/:count?/:region?', async (request, response) => {
     let { count, region } = request.params
     const queryOptions = {}
@@ -103,7 +164,7 @@ dailyRouter.get('/active/:count?', async (request, response) => {
                         Change: cells[3].textContent.trim(),
                         PercentChange: cells[4].textContent.trim(),
                         Volume: cells[5].textContent.trim(),
-                        AveVol: cells[6].textContent.trim(),
+                        AvgVol: cells[6].textContent.trim(),
                         MarketCap: cells[7].textContent.trim(),
                         PE: cells[8].textContent.trim()
                     };
