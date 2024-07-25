dee4fe7f043b18074172cdee30e65a7acf7ae9ef
dnguye12 <duchuyng051@gmail.com>
2024-07-25 16:26:18 +0200

    api endpoint for dividends data

--- controllers/chart.js
+++ controllers/chart.js
@@ -1,6 +1,15 @@
 const yahooFinance = require('yahoo-finance2').default
 const chartRouter = require('express').Router()
 
+chartRouter.get('/:ticker/div', async (request, response) => {
+    const {ticker} = request.params
+    const queryOptions = {period1: '1990-01-01', events: "div"}
+    const result = await yahooFinance.chart(ticker, queryOptions)
+    if(result) {
+        response.json(result)
+    }
+})
+
 chartRouter.get('/:ticker/:period1/:period2?/:intervals?', async (request, response) => {
     const { ticker, period1, period2, intervals } = request.params;
     const queryOptions = { period1: period1 }
